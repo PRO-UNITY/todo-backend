5ec417b9957d852ef618c8b87479eb90bbeb68fb
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2024-01-24 10:06:54 -0500

    success

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
